65f46f500a049c27812c677da6684dc3ab6be4e8
Ryan Goulding <goulding@layerzerolabs.org>
2024-06-26 11:31:36 -0400

    fix:  configurator queue capacity must be a divisor of 2e16
    Signed-off-by: Ryan Goulding <goulding@layerzerolabs.org>

--- packages/stg-evm-v2/deploy/003-deploy-message.ts
+++ packages/stg-evm-v2/deploy/003-deploy-message.ts
@@ -2,6 +2,8 @@ import 'hardhat-deploy'
 
 import '@nomiclabs/hardhat-ethers'
 
+import assert from 'assert'
+
 import { getNetworkConfig } from '@stargatefinance/stg-definitions-v2'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 
@@ -10,6 +12,9 @@ import { createModuleLogger } from '@layerzerolabs/io-devtools'
 
 import { CONTRACT_MESSAGING_TAGS } from '../ts-src'
 import { createDeploy, getFeeData } from '../ts-src/utils/deployments'
+
+const isDivisorOf = (value: number, divisor: number) => value % divisor === 0
+const isDivisorOf2e16 = (divisor: number) => isDivisorOf(2e16, divisor)
 
 export default async function (hre: HardhatRuntimeEnvironment) {
     const deploy = createDeploy(hre)
@@ -22,6 +27,7 @@ export default async function (hre: HardhatRuntimeEnvironment) {
 
     const endpointDeployment = await hre.deployments.get('EndpointV2')
     const queueCapacity = networkConfig.queueCapacity
+    assert(isDivisorOf2e16(queueCapacity), `Queue capacity must be a divisor of 2e16 (65536), got: ${queueCapacity}`)
 
     logger.info(`Deploying TokenMessaging as: ${deployer}`)
     logger.info(`Deploying TokenMessaging with endpoint: ${endpointDeployment.address}`)
