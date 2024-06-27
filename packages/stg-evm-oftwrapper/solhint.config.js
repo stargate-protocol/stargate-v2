e51c277392c4138a261da6fec8cf0f85a65d962c
ChadChillBro7 <trileyschwarz@gmail.com>
2024-06-27 12:45:09 -0700

    Remove old oft wrapper package

--- packages/stg-evm-oftwrapper/solhint.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    extends: ['solhint:recommended', require.resolve('@layerzerolabs/solhint-config')],
-    rules: {
-        'max-line-length': 'warn',
-    },
-};
--- packages/stg-evm-oftwrapper/src/mocks/RevertingInit.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract RevertingInit {
-    function init() external pure {
-        revert();
-    }
-}
