98b956f9758c69409117255d8b6dc10a10bf587d
Ryan Goulding <goulding@layerzerolabs.org>
2024-07-03 15:04:28 -0400

    chore:  make test more realistic by using uint16 capacity
    Get rid of th epointless assumption.
    
    Signed-off-by: Ryan Goulding <goulding@layerzerolabs.org>

--- packages/stg-evm-v2/test/unitest/messaging/Messaging.Token.t.sol
+++ packages/stg-evm-v2/test/unitest/messaging/Messaging.Token.t.sol
@@ -119,8 +119,7 @@ contract TokenMessagingTest is Test {
         assertEq(messaging.nativeDropAmounts(_eid), _nativeDropAmount);
     }
 
-    function test_constructor(uint8 capacity) public {
-        vm.assume(capacity < 200);
+    function test_constructor(uint16 capacity) public {
         if (capacity < 2 || 2 ** 16 % capacity != 0) {
             // cannot use expectRevert when the revert happens in the parent constructor
             try new MockTokenMessaging(LzUtil.deployEndpointV2(MESSAGING_EID, address(this)), address(this), capacity) {
