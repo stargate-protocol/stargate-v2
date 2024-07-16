028d071b1a312ade20c639d39b8d4134a2c2dbf2
ChadChillBro7 <trileyschwarz@gmail.com>
2024-07-16 13:57:56 -0700

    Remove old oft wrappers

--- packages/stg-evm-v2/hardhat.config.ts
+++ packages/stg-evm-v2/hardhat.config.ts
@@ -227,7 +227,7 @@ const networks: NetworksUserConfig = {
     },
     'flare-mainnet': {
         eid: EndpointId.FLARE_V2_MAINNET,
-        url: process.env.RPC_URL_FLARE_MAINNET || 'https://flare.rpc.thirdweb.com',
+        url: process.env.RPC_URL_FLARE_MAINNET || 'https://flare-api.flare.network/ext/C/rpc',
         accounts: mainnetAccounts,
         safeConfig: getSafeConfig(EndpointId.FLARE_V2_MAINNET),
     },
