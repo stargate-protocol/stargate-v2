d1f543e2737904d27de2fdf27315bd60d5af3ee9
ravinagill15 <ravina@layerzerolabs.org>
2024-10-23 13:58:40 -0700

    exporting named module

--- packages/stg-evm-usdt-v2/index.ts
+++ packages/stg-evm-usdt-v2/index.ts
@@ -4,11 +4,15 @@ import { join } from 'path';
 const openZeppelinDir = join(__dirname, '.openzeppelin');
 const jsonFiles = readdirSync(openZeppelinDir).filter(file => file.endsWith('.json'));
 
+const exportsObj: any = {};
+
 // Export each USDT JSON file dynamically
 jsonFiles.forEach(file => {
     const filePath = join(openZeppelinDir, file);
     const jsonData = require(filePath);
     const exportName = file.replace('.json', '');
-    
-    exports[exportName] = jsonData;
+
+    exportsObj[exportName] = jsonData;
 });
+
+export const sepolia = exportsObj.sepolia; // TODO export relevant deployment files after deploying
