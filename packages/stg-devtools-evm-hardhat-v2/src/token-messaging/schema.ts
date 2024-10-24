45f28c1e002315bbbe27b8c27d259bb422cddf84
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2024-10-24 12:51:23 -0700

    🪲 Fix deployments export (#152)

--- packages/stg-devtools-evm-hardhat-v2/src/token-messaging/schema.ts
+++ packages/stg-devtools-evm-hardhat-v2/src/token-messaging/schema.ts
@@ -1,12 +1,22 @@
-import { TokenMessagingEdgeConfigSchema, TokenMessagingNodeConfigSchema } from '@stargatefinance/stg-devtools-v2'
+import {
+    type TokenMessagingEdgeConfig,
+    TokenMessagingEdgeConfigSchema,
+    type TokenMessagingNodeConfig,
+    TokenMessagingNodeConfigSchema,
+} from '@stargatefinance/stg-devtools-v2'
 
 import {
+    type OmniGraphHardhat,
     createOmniEdgeHardhatSchema,
     createOmniGraphHardhatSchema,
     createOmniNodeHardhatSchema,
 } from '@layerzerolabs/devtools-evm-hardhat'
 
-export const TokenMessagingOmniGraphHardhatSchema = createOmniGraphHardhatSchema(
+export const TokenMessagingOmniGraphHardhatSchema: Zod.ZodSchema<
+    OmniGraphHardhat<TokenMessagingNodeConfig, TokenMessagingEdgeConfig>,
+    Zod.ZodTypeDef,
+    unknown
+> = createOmniGraphHardhatSchema(
     createOmniNodeHardhatSchema(TokenMessagingNodeConfigSchema),
     createOmniEdgeHardhatSchema(TokenMessagingEdgeConfigSchema)
 )
