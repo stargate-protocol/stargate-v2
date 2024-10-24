94c0afd619771240a43a7b0e07c1bfad35c6903b
Jan Nanista <jan.jakub.nanista@gmail.com>
2024-10-24 11:44:09 -0700

    chore: Quick workaround for version conflicts

--- packages/stg-devtools-evm-hardhat-v2/src/credit-messaging/schema.ts
+++ packages/stg-devtools-evm-hardhat-v2/src/credit-messaging/schema.ts
@@ -1,12 +1,22 @@
-import { CreditMessagingEdgeConfigSchema, CreditMessagingNodeConfigSchema } from '@stargatefinance/stg-devtools-v2'
+import {
+    type CreditMessagingEdgeConfig,
+    CreditMessagingEdgeConfigSchema,
+    type CreditMessagingNodeConfig,
+    CreditMessagingNodeConfigSchema,
+} from '@stargatefinance/stg-devtools-v2'
 
 import {
+    type OmniGraphHardhat,
     createOmniEdgeHardhatSchema,
     createOmniGraphHardhatSchema,
     createOmniNodeHardhatSchema,
 } from '@layerzerolabs/devtools-evm-hardhat'
 
-export const CreditMessagingOmniGraphHardhatSchema = createOmniGraphHardhatSchema(
+export const CreditMessagingOmniGraphHardhatSchema: Zod.ZodSchema<
+    OmniGraphHardhat<CreditMessagingNodeConfig, CreditMessagingEdgeConfig>,
+    Zod.ZodTypeDef,
+    unknown
+> = createOmniGraphHardhatSchema(
     createOmniNodeHardhatSchema(CreditMessagingNodeConfigSchema),
     createOmniEdgeHardhatSchema(CreditMessagingEdgeConfigSchema)
 )
