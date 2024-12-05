9a163165fc57f11459c5b908ea182f1226bd24fe
lzLamps <135649375+lzLamps@users.noreply.github.com>
2024-12-05 23:35:55 +0000

    USDC Address

--- packages/stg-definitions-v2/src/constant.ts
+++ packages/stg-definitions-v2/src/constant.ts
@@ -483,7 +483,7 @@ export const ASSETS: Record<TokenName, AssetConfig> = {
             },
             [EndpointId.ISLANDER_V2_MAINNET]: {
                 type: StargateType.Oft,
-                //TODO address: '',
+                address: '0xF1815bd50389c46847f0Bda824eC8da914045D14',
             },
             [EndpointId.KLAYTN_V2_MAINNET]: {
                 type: StargateType.Oft,
