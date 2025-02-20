8d34bcee89c476925fb6359542ff282e1f5f6a8a
ravinagill15 <ravina@layerzerolabs.org>
2025-02-19 18:58:21 -0800

    FeelibV1USDC ownership config fix (#315)

---
+++ .changeset/curly-countries-develop.md
@@ -0,0 +1,5 @@
+---
+"@stargatefinance/stg-evm-v2": patch
+---
+
+Fixed sonic ownership config
--- packages/stg-evm-v2/devtools/config/mainnet/01/feelib-v1.usdc.config.ts
+++ packages/stg-evm-v2/devtools/config/mainnet/01/feelib-v1.usdc.config.ts
@@ -40,6 +40,7 @@ import {
     onScroll,
     onSei,
     onSoneium,
+    onSonic,
     onStory,
     onSuperposition,
     onTaiko,
@@ -92,6 +93,7 @@ export default async (): Promise<OmniGraphHardhat<FeeLibV1NodeConfig, FeeLibV1Ed
     const scrollFeeLibV1 = onScroll(contract)
     const seiFeeLibV1 = onSei(contract)
     const soneiumFeeLibV1 = onSoneium(contract)
+    const sonicFeeLibV1 = onSonic(contract)
     const storyFeeLibV1 = onStory(contract)
     const superpositionFeeLibV1 = onSuperposition(contract)
     const taikoFeeLibV1 = onTaiko(contract)
@@ -238,6 +240,10 @@ export default async (): Promise<OmniGraphHardhat<FeeLibV1NodeConfig, FeeLibV1Ed
             },
             {
                 contract: soneiumFeeLibV1,
+                config: defaultNodeConfig,
+            },
+            {
+                contract: sonicFeeLibV1,
                 config: defaultNodeConfig,
             },
             {
