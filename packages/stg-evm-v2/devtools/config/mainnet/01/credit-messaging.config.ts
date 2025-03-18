6583d510c5bb431b6b834b0cc36e414edcccf7e2
Claudia <claudiabarcelovaldes40@gmail.com>
2025-03-18 14:06:28 +0100

    feat: refactor credit message

--- packages/stg-evm-v2/devtools/config/mainnet/01/credit-messaging.config.ts
+++ packages/stg-evm-v2/devtools/config/mainnet/01/credit-messaging.config.ts
@@ -2,8 +2,8 @@ import { CreditMessagingEdgeConfig, CreditMessagingNodeConfig } from '@stargatef
 
 import { OmniGraphHardhat, createGetHreByEid } from '@layerzerolabs/devtools-evm-hardhat'
 
-import { filterConnections, generateCreditMessagingConfig, getSafeAddress } from '../../utils'
-import { getContracts, isValidCreditMessagingChain, validCreditMessagingChains } from '../utils'
+import { filterConnections, generateCreditMessagingConfig, getContractWithEid, getSafeAddress } from '../../utils'
+import { getChainsThatSupportMessaging, isValidChain } from '../utils'
 
 import { DEFAULT_PLANNER } from './constants'
 import { getMessagingAssetConfig } from './shared'
@@ -11,26 +11,47 @@ import { getMessagingAssetConfig } from './shared'
 const contract = { contractName: 'CreditMessaging' }
 
 export default async (): Promise<OmniGraphHardhat<CreditMessagingNodeConfig, CreditMessagingEdgeConfig>> => {
-    const fromChains = process.env.FROM_CHAINS ? process.env.FROM_CHAINS.split(',') : [...validCreditMessagingChains]
-    const toChains = process.env.TO_CHAINS ? process.env.TO_CHAINS.split(',') : [...validCreditMessagingChains]
+    const fromChains = process.env.FROM_CHAINS ? process.env.FROM_CHAINS.split(',') : ''
+    const toChains = process.env.TO_CHAINS ? process.env.TO_CHAINS.split(',') : ''
 
-    console.log('CREDIT_MESSAGING FROM_CHAINS:', fromChains)
-    console.log('CREDIT_MESSAGING TO_CHAINS:', toChains)
-
-    let fromContracts
-    let toContracts
-    try {
-        fromContracts = getContracts(fromChains, contract, isValidCreditMessagingChain)
-        toContracts = getContracts(toChains, contract, isValidCreditMessagingChain)
-    } catch (error) {
-        console.error('Error getting contracts: ', error)
-        throw error
+    // check if all chains are valid
+    if (toChains) {
+        toChains.forEach((chain) => {
+            if (!isValidChain(chain)) {
+                throw new Error(`Invalid chain: ${chain}`)
+            }
+        })
     }
+    if (fromChains) {
+        fromChains.forEach((chain) => {
+            if (!isValidChain(chain)) {
+                throw new Error(`Invalid chain: ${chain}`)
+            }
+        })
+    }
+
+    // get valid chains in the chainsList
+    const supportedChains = getChainsThatSupportMessaging()
+    const validFromChains = fromChains
+        ? supportedChains.filter((chain) => fromChains.includes(chain.name))
+        : supportedChains
+    const validToChains = toChains ? supportedChains.filter((chain) => toChains.includes(chain.name)) : supportedChains
+
+    console.log(
+        'CREDIT_MESSAGING FROM_CHAINS:',
+        validFromChains.map((chain) => chain.name)
+    )
+    console.log(
+        'CREDIT_MESSAGING TO_CHAINS:',
+        validToChains.map((chain) => chain.name)
+    )
+
+    const fromContracts = validFromChains.map((chain) => getContractWithEid(chain.eid, contract))
+    const toContracts = validToChains.map((chain) => getContractWithEid(chain.eid, contract))
 
     const contractMap = new Map()
 
     fromContracts.forEach((contract) => contractMap.set(contract.eid, contract))
-
     toContracts.forEach((contract) => contractMap.set(contract.eid, contract))
 
     const allContracts = Array.from(contractMap.values())
