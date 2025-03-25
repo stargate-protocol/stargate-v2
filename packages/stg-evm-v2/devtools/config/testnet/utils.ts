a81078981920e57623a0933983cc3a14f758d046
Claudia <claudiabarcelovaldes40@gmail.com>
2025-03-25 12:03:19 +0100

    feat: remove bl3 from config files and from hh config

--- packages/stg-evm-v2/devtools/config/testnet/utils.ts
+++ packages/stg-evm-v2/devtools/config/testnet/utils.ts
@@ -9,7 +9,6 @@ export const onArb = withEid(EndpointId.ARBSEP_V2_TESTNET)
 export const onAvalanche = withEid(EndpointId.AVALANCHE_V2_TESTNET)
 export const onOpt = withEid(EndpointId.OPTSEP_V2_TESTNET)
 export const onKlaytn = withEid(EndpointId.KLAYTN_V2_TESTNET)
-export const onBL3 = withEid(EndpointId.BL3_V2_TESTNET)
 export const onOdyssey = withEid(EndpointId.ODYSSEY_V2_TESTNET)
 export const onMantle = withEid(EndpointId.MANTLESEP_V2_TESTNET)
 export const onMonad = withEid(EndpointId.MONAD_V2_TESTNET)
@@ -17,7 +16,6 @@ export const onMonad = withEid(EndpointId.MONAD_V2_TESTNET)
 export const chainEids = {
     'avalanche-testnet': EndpointId.AVALANCHE_V2_TESTNET,
     'arbsep-testnet': EndpointId.ARBSEP_V2_TESTNET,
-    'bl3-testnet': EndpointId.BL3_V2_TESTNET,
     'bsc-testnet': EndpointId.BSC_V2_TESTNET,
     'klaytn-testnet': EndpointId.KLAYTN_V2_TESTNET,
     'mantle-testnet': EndpointId.MANTLESEP_V2_TESTNET,
