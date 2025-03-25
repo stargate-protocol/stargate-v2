56d0113f45810e890662854dd0247c1afc8f15e5
Claudia <claudiabarcelovaldes40@gmail.com>
2025-03-25 20:27:00 +0100

    fix: message

--- packages/stg-devtools-v2/src/messaging/config.ts
+++ packages/stg-devtools-v2/src/messaging/config.ts
@@ -15,7 +15,7 @@ import { IMessaging, MessagingOmniGraph } from './types'
 
 export type MessagingConfigurator = Configurator<MessagingOmniGraph, IMessaging>
 
-const createMessagingLogger = () => createModuleLogger('CreditMessaging')
+const createMessagingLogger = () => createModuleLogger('Messaging')
 const withAsyncLogger = createWithAsyncLogger(createMessagingLogger)
 
 export const configurePlanner: MessagingConfigurator = withAsyncLogger(
