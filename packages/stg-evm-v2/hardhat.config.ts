d3673b62993fa889a9035c0ad802208e22c5c657
Claudia <claudiabarcelovaldes40@gmail.com>
2025-04-02 12:51:47 +0200

    feat: add chain config

--- packages/stg-evm-v2/hardhat.config.ts
+++ packages/stg-evm-v2/hardhat.config.ts
@@ -596,6 +596,13 @@ const networks: NetworksUserConfig = {
         safeConfig: getSafeConfig(EndpointId.XCHAIN_V2_MAINNET),
         timeout: DEFAULT_NETWORK_TIMEOUT,
     },
+    'xdc-mainnet': {
+        eid: EndpointId.XDC_V2_MAINNET,
+        url: process.env.RPC_URL_XDC_MAINNET || 'https://erpc.xinfin.network',
+        accounts: mainnetAccounts,
+        safeConfig: getSafeConfig(EndpointId.XDC_V2_MAINNET),
+        timeout: DEFAULT_NETWORK_TIMEOUT,
+    },
     'zkatana-mainnet': {
         eid: EndpointId.ZKATANA_V2_MAINNET,
         url: process.env.ZKATANA_V2_MAINNET || 'https://rpc.startale.com/astar-zkevm',
