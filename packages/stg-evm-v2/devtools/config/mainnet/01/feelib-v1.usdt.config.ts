d74481c75dfc0c8d5a1fdd00dacf84e0f1d3ac7d
Claudia <claudiabarcelovaldes40@gmail.com>
2025-05-30 13:44:18 +0200

    ci: create a building graph helper function for fee libs and reuse it in each fee libs v1 config

--- packages/stg-evm-v2/devtools/config/mainnet/01/feelib-v1.usdt.config.ts
+++ packages/stg-evm-v2/devtools/config/mainnet/01/feelib-v1.usdt.config.ts
@@ -4,34 +4,15 @@ import { FeeLibV1EdgeConfig, FeeLibV1NodeConfig } from '@stargatefinance/stg-dev
 import { OmniGraphHardhat } from '@layerzerolabs/devtools-evm-hardhat'
 
 import { getFeeLibV1DeployName } from '../../../../ops/util'
-import { getContractWithEid } from '../../utils'
-import { filterValidProvidedChains, getChainsThatSupportToken } from '../utils'
 
-import { DEFAULT_PLANNER } from './constants'
+import buildFeeLibV1DeploymentGraph from './feelib-v1.config.utils'
 
 const tokenName = TokenName.USDT
 const contract = { contractName: getFeeLibV1DeployName(tokenName) }
 
 export default async (): Promise<OmniGraphHardhat<FeeLibV1NodeConfig, FeeLibV1EdgeConfig>> => {
-    const defaultNodeConfig = {
-        owner: DEFAULT_PLANNER,
-    }
-
     // only use the chains defined in the env variable if it is set
     const chainsList = process.env.CHAINS_LIST ? process.env.CHAINS_LIST.split(',') : []
 
-    // get valid chains config in the chainsList
-    const validChains = filterValidProvidedChains(chainsList, getChainsThatSupportToken(tokenName))
-
-    const contracts = Array.from(validChains).map((chain) => {
-        return {
-            contract: getContractWithEid(chain.eid, contract),
-            config: defaultNodeConfig,
-        }
-    })
-
-    return {
-        contracts,
-        connections: [],
-    }
+    return buildFeeLibV1DeploymentGraph(tokenName, contract, chainsList)
 }
