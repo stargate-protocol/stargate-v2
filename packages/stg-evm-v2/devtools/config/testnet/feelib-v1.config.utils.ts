72d84940b7d22414b24ca23b62fb129b7b231d26
Claudia <claudiabarcelovaldes40@gmail.com>
2025-08-13 13:42:05 +0200

    feat: update feelib v1 config files for all assets

---
+++ packages/stg-evm-v2/devtools/config/testnet/feelib-v1.config.utils.ts
@@ -0,0 +1,44 @@
+import { TokenName } from '@stargatefinance/stg-definitions-v2'
+import { FeeLibV1EdgeConfig, FeeLibV1NodeConfig } from '@stargatefinance/stg-devtools-v2'
+
+import { OmniGraphHardhat } from '@layerzerolabs/devtools-evm-hardhat'
+
+import { getFeeLibV1DeployName } from '../../../ops/util'
+import { getContractWithEid } from '../utils'
+import { filterValidProvidedChains, getChainsThatSupportToken, printChains } from '../utils.config'
+
+import { DEFAULT_PLANNER } from './constants'
+import { setTestnetStage } from './utils'
+
+export default async function buildFeeLibV1DeploymentGraph(
+    tokenName: TokenName
+): Promise<OmniGraphHardhat<FeeLibV1NodeConfig, FeeLibV1EdgeConfig>> {
+    // Set the stage to testnet
+    setTestnetStage()
+
+    const contract = { contractName: getFeeLibV1DeployName(tokenName) }
+
+    // only use the chains defined in the env variable if it is set
+    const chainsList = process.env.CHAINS_LIST ? process.env.CHAINS_LIST.split(',') : []
+
+    const defaultNodeConfig = {
+        owner: DEFAULT_PLANNER,
+    }
+
+    // get valid chains config in the chainsList
+    const validChains = filterValidProvidedChains(chainsList, getChainsThatSupportToken(tokenName))
+
+    printChains(`feelib_v1.${tokenName} CHAINS_LIST:`, validChains)
+
+    const contracts = Array.from(validChains).map((chain) => {
+        return {
+            contract: getContractWithEid(chain.eid, contract),
+            config: defaultNodeConfig,
+        }
+    })
+
+    return {
+        contracts,
+        connections: [],
+    }
+}
