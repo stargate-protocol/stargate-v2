cb85a835492d912c8fa5fd25972c5f804d490991
ravinagill15 <ravina@layerzerolabs.org>
2025-08-21 17:39:52 -0700

    removed unneeded hardhat config

--- packages/stg-evm-sdk-v2/hardhat.config.ts
+++
@@ -1,9 +0,0 @@
-// Minimal hardhat configuration to provide type definitions for ts-node
-import 'hardhat-deploy'
-
-const config = {
-    networks: {},
-    solidity: '0.8.22',
-}
-
-export default config
