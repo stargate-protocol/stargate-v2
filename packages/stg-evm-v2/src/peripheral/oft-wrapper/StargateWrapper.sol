ce4b12bac8b965cdc5115794b7991f89ba530ef7
Claudia <claudiabarcelovaldes40@gmail.com>
2025-10-17 18:55:02 +0200

    feat: enhance wrapper

--- packages/stg-evm-v2/src/peripheral/oft-wrapper/StargateWrapper.sol
+++ packages/stg-evm-v2/src/peripheral/oft-wrapper/StargateWrapper.sol
@@ -12,16 +12,7 @@ import { IStargateWrapper } from "./interfaces/IStargateWrapper.sol";
 contract StargateWrapper is IStargateWrapper, Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    uint256 public constant BPS_DENOMINATOR = 10_000;
-    address public constant NOT_PARTNER_ADDRESS = address(1);
-
-    // ! could make sense to have a list of allowed signers? in case we want more than one api using the same wrapper?
-    // ! note this will need a more careful maintenance of the signers list since old one should be disabled/removed
     address public externalSigner;
-
-    // Protocol fee
-    uint256 public defaultBPS;
-    mapping(address ctrAddress => uint256 bps) public specificBPS;
 
     // Partners
     mapping(uint256 partnerId => Partner partner) public partners;
@@ -29,27 +20,13 @@ contract StargateWrapper is IStargateWrapper, Ownable, ReentrancyGuard {
     // @dev partnerId = 0 will be used for the protocol fees
     mapping(uint256 partnerId => mapping(address oft => uint256 bps)) public accruedFees;
 
-    constructor(uint256 defaultBPS_, address owner_) {
-        defaultBPS = defaultBPS_;
+    constructor(address owner_) {
         _transferOwnership(owner_);
     }
 
     modifier onlyActivePartner(uint256 partnerId) {
         if (!partners[partnerId].isActive) revert PartnerNotActive(partnerId);
         _;
-    }
-
-    // BPS fees
-    function setDefaultBPS(uint256 defaultBPS_) external onlyOwner {
-        defaultBPS = defaultBPS_;
-
-        emit ProtocolDefaultBpsSet(defaultBPS);
-    }
-
-    function setBpsByOFT(address oft, uint256 bps) external onlyOwner {
-        specificBPS[oft] = bps;
-
-        emit ProtocolBpsByOFTSet(oft, bps);
     }
 
     // Partners
@@ -68,28 +45,28 @@ contract StargateWrapper is IStargateWrapper, Ownable, ReentrancyGuard {
         emit PartnerPaused(partnerId);
     }
 
+    /**
+     * ! TBD
+     * !  - Should the partner be able to withdraw their fees?
+     * !  - Should the withdrawn fees for paused partner?
+     * !  - should allow withdrawing a specific amount of fees?
+     * !  - Should the partner be able to withdraw the fees to a specific address?
+     * @param partnerId The ID of the partner
+     * @dev only the owner can withdraw the fees
+     */
+
     function withdrawPartnerAccruedFees(
         uint256 partnerId,
-        address oft,
-        address receiver
-    ) external onlyActivePartner(partnerId) {
-        // check if the caller is authorized to withdraw
-        address authorizedWithdrawAddress = partners[partnerId].partnerAddress;
-        if (authorizedWithdrawAddress == NOT_PARTNER_ADDRESS) {
-            // if there is no authorized withdraw address, only the protocol owner can withdraw
-            if (msg.sender != owner()) revert WithdrawNotAuthorized(partnerId, msg.sender);
-        } else {
-            if (msg.sender != authorizedWithdrawAddress) revert WithdrawNotAuthorized(partnerId, msg.sender);
-        }
-
-        // ! should allow withdrawing an specific amount of accruedFees?
+        address oft
+    ) external onlyActivePartner(partnerId) onlyOwner {
         uint256 accruedBps = accruedFees[partnerId][oft];
+
         if (accruedBps == 0) return;
 
         accruedFees[partnerId][oft] = 0;
-        IERC20(oft).safeTransfer(receiver, accruedBps);
+        IERC20(oft).safeTransfer(partners[partnerId].partnerAddress, accruedBps);
 
-        emit PartnerAccruedFeesWithdrawn(partnerId, oft, receiver, accruedBps);
+        emit PartnerAccruedFeesWithdrawn(partnerId, oft, partners[partnerId].partnerAddress, accruedBps);
     }
 
     function sweep(address receiver, address[] calldata tokens) external onlyOwner {
@@ -99,79 +76,93 @@ contract StargateWrapper is IStargateWrapper, Ownable, ReentrancyGuard {
     }
 
     // Send
-    function sendErc20(
-        PartnerFee calldata partnerFee,
+    /**
+     * @dev only active partners can send operations
+     * ! TODO
+     * ! - Check the gas and operation params are enough to cover all operation costs
+     * !         - CCTP fees - cctp fees + protocol fees + partner fees + the autoclaim fees must be paid
+     * !         - Ask multihop particularities.
+     * ! - This assumes the protocol and partner fees are paid in the same operation token, explore the possibility of having different tokens for each fee.
+     * ! - Check if there is any missing validations.
+     */
+    function send(
+        uint256 partnerId,
         OperationParams calldata operationParams,
         GasOperationParams calldata gasOperationParams,
         Call calldata call,
         bytes calldata signature
-    ) external payable nonReentrant {
-        _validateCall(call);
-        // todo the payload should be all received params encoded together, do any adjustments needed?
+    ) external payable nonReentrant onlyActivePartner(partnerId) {
+        // validations
+        bool opInNative = operationParams.token == address(0);
+        bool gasOpInNative = gasOperationParams.token == address(0);
+
+        // todo the payload should be all received params encoded together, do any adjustments needed
         _validateSignature(
-            abi.encode(address(this), block.chainid, partnerFee, operationParams, gasOperationParams, call),
+            abi.encode(address(this), block.chainid, partnerId, operationParams, gasOperationParams, call),
             signature
         );
         _validateOperationParams(operationParams);
+        _validateCall(call);
+        _validateMsgValue(
+            opInNative,
+            gasOpInNative,
+            operationParams.totalAmount,
+            gasOperationParams.amount,
+            call.value
+        );
 
-        // partner fee and protocol fee
-        _validateFee(partnerFee.bps, operationParams.amountToSend, operationParams.partnerFee);
-        uint256 protocolBPS = specificBPS[operationParams.token];
-        if (protocolBPS == 0) protocolBPS = defaultBPS;
-        _validateFee(protocolBPS, operationParams.amountToSend, operationParams.protocolFee);
-
-        // accumulate the partner and protocol fees
-        accruedFees[partnerFee.partnerId][operationParams.token] += operationParams.partnerFee;
+        // accumulate the protocol and partner fees
+        accruedFees[partnerId][operationParams.token] += operationParams.partnerFee;
         accruedFees[0][operationParams.token] += operationParams.protocolFee;
 
-        emit OperationSent(
-            msg.sender,
-            operationParams.partnerFee,
-            operationParams.protocolFee,
-            operationParams.token,
-            msg.value - call.value
-        );
+        // get funds from user to do the operation
+        if (!opInNative) {
+            IERC20(operationParams.token).safeTransferFrom(msg.sender, address(this), operationParams.totalAmount);
 
-        emit WrapperFees(
-            partnerFee.partnerId,
-            operationParams.token,
-            operationParams.partnerFee,
-            operationParams.protocolFee
-        );
-
-        // get the funds from the user to do the operation
-        IERC20(operationParams.token).safeTransferFrom(msg.sender, address(this), operationParams.amountToSend);
-        if (gasOperationParams.token != address(0)) {
-            // if the gas must be payed in ERC20
-            IERC20(gasOperationParams.token).safeTransferFrom(msg.sender, address(this), gasOperationParams.amount);
+            // approve the contracts to spend the funds
+            IERC20(operationParams.token).safeApprove(operationParams.ctrAddress, operationParams.amountToSend);
         }
 
-        IERC20(operationParams.token).safeApprove(operationParams.ctrAddress, operationParams.amountToSend);
-        IERC20(gasOperationParams.token).safeApprove(gasOperationParams.ctrAddress, gasOperationParams.amount);
+        if (!gasOpInNative) {
+            IERC20(gasOperationParams.token).safeTransferFrom(msg.sender, address(this), gasOperationParams.amount);
 
+            // approve the contracts to spend the funds
+            IERC20(gasOperationParams.token).safeApprove(gasOperationParams.ctrAddress, gasOperationParams.amount);
+        }
+
+        // do the wrapped operation
         (bool success, bytes memory result) = call.to.call{ value: call.value }(call.data);
-
         if (!success) revert OperationFailed(string(result));
 
-        // ! is this needed?
-        IERC20(operationParams.token).safeApprove(operationParams.ctrAddress, 0);
-        IERC20(gasOperationParams.token).safeApprove(gasOperationParams.ctrAddress, 0);
-    }
-
-    function sendNative(
-        PartnerFee calldata partnerFee,
-        OperationParams calldata operationParams,
-        // ! it is not using the gasOperationParams but I think it might be still needed (e.x the fees are payed in LZ token)
-        GasOperationParams calldata gasOperationParams,
-        Call calldata call,
-        bytes calldata signature
-    ) external payable nonReentrant {
-        // todo
+        // emit the events
+        // todo check any other value that should be emitted
+        emit OperationSent(msg.sender, partnerId, call.to, operationParams.amountToSend);
+        emit WrapperFees(
+            partnerId,
+            operationParams.token,
+            operationParams.partnerFee,
+            operationParams.protocolFee,
+            gasOperationParams.token,
+            gasOperationParams.amount
+        );
     }
 
     function _validateCall(Call calldata call) internal view {
         if (call.to != address(this)) revert CallToSelf();
-        if (msg.value >= call.value) revert InvalidMsgValue(msg.value, call.value);
+    }
+
+    function _validateMsgValue(
+        bool opInNative,
+        bool gasOpInNative,
+        uint256 opTotalAmount,
+        uint256 gasOpAmount,
+        uint256 callValue
+    ) internal view {
+        uint256 expectedValue = callValue;
+        if (opInNative) expectedValue += opTotalAmount;
+        if (gasOpInNative) expectedValue += gasOpAmount;
+
+        if (msg.value < expectedValue) revert InvalidMsgValue(msg.value, expectedValue);
     }
 
     function _validateSignature(bytes memory payload, bytes calldata signature) internal view {
@@ -180,17 +171,19 @@ contract StargateWrapper is IStargateWrapper, Ownable, ReentrancyGuard {
     }
 
     function _validateOperationParams(OperationParams calldata operationParams) internal pure {
-        /**
-         * todo validate operation params
-         * todo validate the contracts addresses is not zero
-         * todo validate the amount in operation is not zero
-         * ...
-         *
-         * */
+        if (operationParams.token == address(0)) revert InvalidOperationToken(operationParams.token);
+        if (operationParams.ctrAddress == address(0)) revert InvalidOperationCtrAddress(operationParams.ctrAddress);
+        if (
+            operationParams.totalAmount ==
+            operationParams.amountToSend + operationParams.protocolFee + operationParams.partnerFee
+        )
+            revert InvalidOperationAmounts(
+                operationParams.totalAmount,
+                operationParams.amountToSend,
+                operationParams.protocolFee,
+                operationParams.partnerFee
+            );
     }
 
-    function _validateFee(uint256 bps, uint256 amount, uint256 feeAmount) internal pure {
-        uint256 calculatedFee = (amount * bps) / BPS_DENOMINATOR;
-        if (calculatedFee != feeAmount) revert InvalidFee(calculatedFee, feeAmount);
-    }
+    function withdrawPartnerAccruedFees(uint256 partnerId, address oft, address receiver) external override {}
 }
