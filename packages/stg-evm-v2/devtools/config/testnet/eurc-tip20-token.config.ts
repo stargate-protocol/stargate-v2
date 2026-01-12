7753836749fc87fa56a91ac8921323ebe012487e
Claudia <claudiabarcelovaldes40@gmail.com>
2026-01-12 23:25:58 +0100

    feat: add config files for usdc and eurc tip 20

---
+++ packages/stg-evm-v2/devtools/config/testnet/eurc-tip20-token.config.ts
@@ -0,0 +1,14 @@
+import { TokenName } from '@stargatefinance/stg-definitions-v2'
+
+import { OmniGraphHardhat } from '@layerzerolabs/devtools-evm-hardhat'
+import { Stage } from '@layerzerolabs/lz-definitions'
+
+import buildTip20TokenGraph from '../utils/tip20-token.config.utils'
+
+import type { Tip20NodeConfig } from '@stargatefinance/stg-devtools-v2'
+
+const tokenName = TokenName.EURC
+
+export default async (): Promise<OmniGraphHardhat<Tip20NodeConfig, unknown>> => {
+    return buildTip20TokenGraph(Stage.TESTNET, tokenName)
+}
