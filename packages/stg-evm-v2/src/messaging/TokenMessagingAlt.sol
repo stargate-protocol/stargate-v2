7f42716f85ace38334d93c15c33e57e7999a8eac
Claudia <claudiabarcelovaldes40@gmail.com>
2026-01-23 00:23:55 +0100

    fix comment

--- packages/stg-evm-v2/src/messaging/TokenMessagingAlt.sol
+++ packages/stg-evm-v2/src/messaging/TokenMessagingAlt.sol
@@ -13,7 +13,7 @@ contract TokenMessagingAlt is TokenMessaging {
     ) TokenMessaging(_endpoint, _owner, _queueCapacity) {}
 
     /// @dev Alt endpoints accept ERC20 fees; block any native value from being forwarded.
-    /// @dev we don't need to check msg.value is not zero because in the StargateOFTAlt._taxi we are already sending no msg.value
+    /// @dev we ignore msg.value not being zero intentionally, because in the StargateOFTAlt._taxi we are already sending no msg.value
     function _payNative(uint256 /*_nativeFee*/) internal pure override returns (uint256 nativeFee) {
         nativeFee = 0;
     }
