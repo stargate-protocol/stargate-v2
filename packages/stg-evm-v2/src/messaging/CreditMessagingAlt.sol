75da68eb0e139998fd7c6663603dd41ac6e9b042
Claudia <claudiabarcelovaldes40@gmail.com>
2026-01-22 19:59:45 +0100

    fix use correct fee

--- packages/stg-evm-v2/src/messaging/CreditMessagingAlt.sol
+++ packages/stg-evm-v2/src/messaging/CreditMessagingAlt.sol
@@ -23,11 +23,12 @@ contract CreditMessagingAlt is CreditMessaging, Transfer {
     }
 
     /// @dev Override _lzSend to quote fee in ALT token and fund the endpoint, then call super._lzSend.
+    /// @dev the _fee received will be { nativeFee: 0, lzTokenFee: 0 } because the msg.value is 0, so it should use the calculated one instead
     function _lzSend(
         uint32 _dstEid,
         bytes memory _message,
         bytes memory _options,
-        MessagingFee memory _fee,
+        MessagingFee memory /*_fee*/,
         address _sender
     ) internal override returns (MessagingReceipt memory receipt) {
         // quote fee in ALT token and fund the endpoint
@@ -35,7 +36,7 @@ contract CreditMessagingAlt is CreditMessaging, Transfer {
         // fund the endpoint with the ERC20 native fee
         if (fee.nativeFee > 0) safeTransferTokenFrom(feeToken, msg.sender, address(endpoint), fee.nativeFee);
 
-        return super._lzSend(_dstEid, _message, _options, _fee, _sender);
+        return super._lzSend(_dstEid, _message, _options, fee, _sender);
     }
 
     /// @dev Override native payment hook so endpoint.send is called with msg.value == 0.
