e2ff7fb9d889563da924ffc7861a7fa7f9ca2340
Claudia Barcelo <claudiabarcelovaldes40@gmail.com>
2026-08-27 14:48:11 +0200

    feat: add canary DVN

--- packages/stg-definitions-v2/src/constant.ts
+++ packages/stg-definitions-v2/src/constant.ts
@@ -3226,12 +3226,20 @@ export const NETWORKS: NetworksConfig = {
     [EndpointId.OPN_V2_MAINNET]: {
         creditMessaging: {
             ...DEFAULT_CREDIT_MESSAGING_NETWORK_CONFIG,
-            requiredDVNs: [DVNS.NETHERMIND[EndpointId.OPN_V2_MAINNET], DVNS.LZ_LABS[EndpointId.OPN_V2_MAINNET]],
+            requiredDVNs: [
+                DVNS.NETHERMIND[EndpointId.OPN_V2_MAINNET],
+                DVNS.LZ_LABS[EndpointId.OPN_V2_MAINNET],
+                DVNS.CANARY[EndpointId.OPN_V2_MAINNET],
+            ],
             executor: EXECUTORS.LZ_LABS[EndpointId.OPN_V2_MAINNET],
         },
         tokenMessaging: {
             ...DEFAULT_TOKEN_MESSAGING_NETWORK_CONFIG,
-            requiredDVNs: [DVNS.NETHERMIND[EndpointId.OPN_V2_MAINNET], DVNS.LZ_LABS[EndpointId.OPN_V2_MAINNET]],
+            requiredDVNs: [
+                DVNS.NETHERMIND[EndpointId.OPN_V2_MAINNET],
+                DVNS.LZ_LABS[EndpointId.OPN_V2_MAINNET],
+                DVNS.CANARY[EndpointId.OPN_V2_MAINNET],
+            ],
             executor: EXECUTORS.LZ_LABS[EndpointId.OPN_V2_MAINNET],
             nativeDropAmount: parseEther('0.3').toBigInt(),
             busDisabled: true,
